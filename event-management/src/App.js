655f18b88840e01854fe8b2f03cdc6cb36dd1aaf
Adiination <Aditya.sasaram11@gmail.com>
2023-06-02 14:29:39 +0530

    created the react boilerplate,made components folder and files and worked on eventlist

---
+++ event-management/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Eventlist from "./components/Eventlist";
+
+function App() {
+  return (
+    <div>
+      {" "}
+      <Eventlist />
+    </div>
+  );
+}
+export default App;
---
+++ event-management/src/components/Eventlist.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Eventlist = () => {
+  return <div>Eventlist</div>;
+};
+
+export default Eventlist;
