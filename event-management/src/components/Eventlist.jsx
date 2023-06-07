b7554a10bbde4af222a9d1a0ad7d29bbdad48805
Adiination <Aditya.sasaram11@gmail.com>
2023-06-07 23:59:24 +0530

    adding routes

--- event-management/src/components/Eventlist.jsx
+++ event-management/src/components/Eventlist.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import EventDetails from "./EventDetails";
 
 function Eventlist() {
-  return <div>Eventlist</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/EventDetails" element={<EventDetails />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Eventlist;
